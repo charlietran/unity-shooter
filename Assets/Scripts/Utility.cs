df1db4876eb3169a59299a64133c572f55caecf7
Charlie Tran <hi@charlietran.com>
2017-10-27 17:16:09 -0400

    Utility.ShuffleArray
    Add a utility method for shuffling a passed-in array of any type, using the Fisher-Yates algorithm

---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Utility {
+
+    // Shuffles an array of any class using the Fisher-Yates algorithm
+    public static T[] ShuffleArray<T>(T[] array, int seed) {
+        System.Random pseudoRNG = new System.Random(seed);
+
+        for(int i = 0; i < array.Length - 1; i++) {
+            int randomIndex = pseudoRNG.Next(i, array.Length);
+            T tempItem = array[randomIndex];
+            array[randomIndex] = array[i];
+            array[i] = tempItem;
+        }
+
+        return array;
+    }
+}
