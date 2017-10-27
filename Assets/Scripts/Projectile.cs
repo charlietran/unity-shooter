560d46c777182b6171ccb572d714525dd142743f
Charlie Tran <hi@charlietran.com>
2017-10-27 04:18:32 -0400

    Projectile collision and lifetime improvements
    Make sure that projectiles have a finite lifetime, so that we don't fill up memory
    
    Use a collisionBufferWidth in our CheckCollisions calculation, to account for the fact that the projectile might end up slightly inside an enemy during a frame (since the bullet and enemy are moving towards each other)
    
    Check for initial collisions, since tour Raycast checking won't work properly if a projectile spawns inside an enemy

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class Projectile : MonoBehaviour {
     float speed = 10;
     float damage = 1;
+    float lifetime = 3.0f;
+    float collisionBufferWidth = 0.1f;
+
     public LayerMask collisionMask;
      
-    public void SetSpeed (float newSpeed)
-    {
+    public void SetSpeed (float newSpeed) {
         speed = newSpeed;
     }
 
-    void Update ()
-    {
+    private void Start() {
+        // Make sure our bullets don't live forever
+        Destroy(gameObject, lifetime);
+        CheckInitialCollisions();
+    }
+
+    void Update () {
         float moveDistance = speed * Time.deltaTime;
 
         // Before we move the projectile, check if it would hit something this frame
@@ -23,8 +30,16 @@ public class Projectile : MonoBehaviour {
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 
-    void CheckCollisions(float moveDistance)
-    {
+    void CheckInitialCollisions() {
+        // Find all the colliders touching our projectile at spawn time
+        Collider[] initialCollisions = Physics.OverlapSphere(transform.position, 0.1f, collisionMask);
+        if (initialCollisions.Length > 0) {
+            OnHitObject(initialCollisions[0]);
+        }
+    }
+
+
+    void CheckCollisions(float moveDistance) {
         // Create a ray at the projectile's current position and orientation
         Ray ray = new Ray(transform.position, transform.forward);
 
@@ -32,24 +47,39 @@ public class Projectile : MonoBehaviour {
         RaycastHit hit;
 
         // Cast a ray to see if we hit any colliders. If we do, store the location in the "hit" var
-        bool hitSomething = Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide);
+        bool hitSomething = Physics.Raycast(
+            ray, 
+            out hit,
+            // We add the collision buffer width here to compensate for the fact that enemies are moving as well
+            // So if the enemy is within the projectile's move distance plus our slight buffer, we call it a hit
+            moveDistance + collisionBufferWidth, 
+            collisionMask, 
+            QueryTriggerInteraction.Collide
+        );
 
         if (hitSomething) {
             OnHitObject(hit);
         }
     }
 
-    void OnHitObject(RaycastHit hit)
-    {
+    void OnHitObject(RaycastHit hit) {
         IDamageable damageableObject = hit.collider.GetComponent<IDamageable>();
-         
-        if (damageableObject != null)
-        {
-            damageableObject.TakeHit(damage, hit);
+        DamageObject(damageableObject);
+    }
+
+    // This overloaded OnHitObject is for CheckInitialCollisions, when we won't have a RaycastHit
+    void OnHitObject(Collider collider) {
+        IDamageable damageableObject = collider.GetComponent<IDamageable>();
+        DamageObject(damageableObject);
+    }
+
+    void DamageObject(IDamageable damageableObject) {
+        if (damageableObject != null) {
+            damageableObject.TakeDamage(damage);
         }
 
         // gameObject is a reference to the current game object (the projectile)
-        // Destroy the projectile once it hits something
+        // Destroy the projectile when it hits something
         GameObject.Destroy (gameObject);
     }
 
