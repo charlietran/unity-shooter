361de7dbf9b364eb40a071022195b93ef5a0589f
Charlie Tran <hi@charlietran.com>
2017-10-31 16:39:08 -0400

    Add accessibility assurance to map generation
    Beyond just setting random tiles to be obstacles, we also want to make sure than an orthogonally accessible path is routed throughout the entire map

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -14,16 +14,15 @@ public class MapGenerator : MonoBehaviour {
     float mapTopBoundary;
     float mapLeftBoundary;
 
-    public int obstacleSeed = 10;
-
-    // This list will hold all possible tile coordinates that obstacles and go into
-    List<Coord> allTileCoords;
-
     // This will be a shuffled queue of all possible tile coordinates
-    Queue<Coord> shuffledTileCoords;
+    Queue<Coord> shuffledPossibleObstacleCoords;
 
-    // How many obstacles to create
-    public int obstacleCount = 10;
+    // The percentage of tiles that will be obstacles
+    [Range(0,1)]
+    public float obstaclePercentage = 0.5f;
+
+    // Arbitrary starting seed for our obstacle randomization
+    public int obstacleSeed = 10;
 
     // This value dictates how much to "shrink" our instantiated tiles
     [Range(0,1)]
@@ -32,11 +31,14 @@ public class MapGenerator : MonoBehaviour {
     // Used for identitifying our generated map so we can easily destroy/recreate it from our custom editor tool
     const string mapHolderName = "Generated Map";
 
+    // This will get set to the center tile of our map. Used for determining map accessibility
+    Coord mapCenter;
+
     void Start() { GenerateMap(); }
 
     // Generates a map of our mapSize from our tilePrefabs
     public void GenerateMap() {
-        InitAllTileCoords();
+        InitPossibleObstacleCoords();
         Transform mapHolder = CreateMapHolder();
 
         float tileWidth = tilePrefab.transform.localScale.x;
@@ -69,12 +71,95 @@ public class MapGenerator : MonoBehaviour {
     }
 
     private void InstantiateObstacles(Transform mapHolder) {
-        for (int i = 0; i < obstacleCount; i++) {
+        // 2D Array that represents our whole map, and which of its tiles are obstacles
+        bool[,] obstacleMap = new bool[(int)mapSize.x, (int)mapSize.y];
+
+        int obstacleMax = (int)(mapSize.x * mapSize.y * obstaclePercentage);
+
+        // Counter of how mnay obstacles we've placed on the map
+        int obstacleCount = 0;
+
+        for (int i = 0; i < obstacleMax; i++) {
+            // Get a random coordinate and try it as an onstacle
             Coord randomCoord = GetRandomCoord();
-            Vector3 obstaclePosition = PositionFromCoord(randomCoord.x, randomCoord.y);
-            Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition + Vector3.up * 0.5f, Quaternion.identity);
-            newObstacle.parent = mapHolder;
+            obstacleMap[randomCoord.x, randomCoord.y] = true;
+            obstacleCount++;
+
+            // Check if the map remains accessible after adding this obstacle
+            bool accessible = MapIsFullyAccessible(obstacleMap, obstacleCount);
+
+            if (accessible) {
+                Vector3 obstaclePosition = PositionFromCoord(randomCoord.x, randomCoord.y);
+                Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition + Vector3.up * 0.5f, Quaternion.identity);
+                newObstacle.parent = mapHolder;
+            } else {
+                obstacleMap[randomCoord.x, randomCoord.y] = false;
+                obstacleCount--;
+            }
         }
+    }
+
+    // Use a flood fill algorithm to make sure no part of the map is blocked off
+    bool MapIsFullyAccessible(bool[,] obstacleMap, int obstacleCount) {
+        int obstacleMapWidth = obstacleMap.GetLength(0);
+        int obstacleMapHeight = obstacleMap.GetLength(1);
+        Queue<Coord> accessibilityValidationQueue = new Queue<Coord>();
+
+        // Keep an array of coordinates that we've already checked
+        bool[,] validatedTiles = new bool[obstacleMapWidth, obstacleMapHeight];
+        validatedTiles[mapCenter.x, mapCenter.y] = true;
+
+        // Start our validation queue with the map center
+        accessibilityValidationQueue.Enqueue(mapCenter);
+        int accessibleTileCount = 1;
+
+        // Our desired number of accessible tiles is the total number of tiles minus the obstacle count
+        int targetAccessibleTileCount = (int)(mapSize.x * mapSize.y - obstacleCount);
+
+        // This while loop is a "flood fill" check for all tile availability. Starting from the center tile,
+        // it checks to make sure none of its orthogonal neighbors is accessible, i.e. not an obstacle or map boundary. 
+        // Then it recursively enqueues each orthogonal coordinate it finds, until the whole map is checked
+        while (accessibilityValidationQueue.Count > 0) {
+            Coord tile = accessibilityValidationQueue.Dequeue();
+
+            // Check each of the eight neighboring tiles to make sure it's not already filled with an obstacle
+            for(int xOffset = -1; xOffset <= 1; xOffset++) {
+                for (int yOffset = -1; yOffset <= 1; yOffset++) {
+                    // Set the coordinates of the neighbor tile that we're checking
+                    int neighborX = tile.x + xOffset;
+                    int neighborY = tile.y + yOffset;
+
+                    bool isValidNeighbor = ValidForAccessibilityCheck(obstacleMapWidth, obstacleMapHeight, xOffset, yOffset, neighborX, neighborY);
+
+                    if (isValidNeighbor) {
+                        // If we haven't validated this neighbor tile already, and if it's also not an obstacle,
+                        // then mark that it's been validated, increase our accessible tile count,
+                        // then queue this tile coordinate 
+                        if(!validatedTiles[neighborX, neighborY] && !obstacleMap[neighborX, neighborY]) {
+                            validatedTiles[neighborX, neighborY] = true;
+                            accessibleTileCount++;
+                            accessibilityValidationQueue.Enqueue(new Coord(neighborX, neighborY));
+                        }
+                    }
+                }
+            }
+        }
+
+        // Our accessibility flood-fill check may reveal that it's no longer possible to add obstacles
+        // while ensuring accessibility. In which case, the returned accessibleTileCount will be lower than
+        // our targetAccessibleTileCount, and we'll return false for the map being fully accessible
+
+        return targetAccessibleTileCount == accessibleTileCount;
+    }
+
+    bool ValidForAccessibilityCheck(int obstacleMapWidth, int obstacleMapHeight, int xOffset, int yOffset, int neighborX, int neighborY) {
+        // We only want to check orthgonally, which is done by ensuring at least one of the offset axes is zero
+        bool orthogonalCheck = (xOffset == 0 || yOffset == 0);
+
+        // Make sure the coordinates of the neighbor tile we're checking are within the bounds of the map
+        bool withinBoundaries = (neighborX >= 0 && neighborY >= 0 && neighborX < obstacleMapWidth && neighborY < obstacleMapHeight);
+
+        return orthogonalCheck && withinBoundaries;
     }
 
 
@@ -82,24 +167,29 @@ public class MapGenerator : MonoBehaviour {
         return new Vector3( mapLeftBoundary + x, 0, mapTopBoundary + y );
     }
 
-    private void InitAllTileCoords() {
-        // Initialize with an empty Coord List
-        allTileCoords = new List<Coord>();
+    private void InitPossibleObstacleCoords() {
+        // This list will hold all possible tile coordinates where obstacles may be instantiated
+        List<Coord> possibleObstacleCoords = new List<Coord>();
 
-        // Fill the list with Coords corresopnding to the dimensions of our map
+        mapCenter = new Coord((int)mapSize.x / 2, (int)mapSize.y / 2);
+
+        // Fill the list with Coords corresponding to the dimensions of our map, excluding the center
+        // (The center tile will always be available as that's the player spawning point)
         for (int tileX = 0; tileX < mapSize.x; tileX++) {
             for (int tileY = 0; tileY < mapSize.y; tileY++) {
-                allTileCoords.Add(new Coord(tileX, tileY));
+                // 
+                if (tileX != mapCenter.x && tileY != mapCenter.y) {
+                    possibleObstacleCoords.Add(new Coord(tileX, tileY));
+                }
             }
         }
 
-        // From the list, create a randomized Coord queue of all possible coords
-        shuffledTileCoords = new Queue<Coord>(
-                                Utility.ShuffleArray(
-                                    allTileCoords.ToArray(),
-                                    obstacleSeed
-                                )
-                             );
+        // From the list, create a randomized queue of all possible obstacle coords
+        shuffledPossibleObstacleCoords = new Queue<Coord>(
+                                            Utility.ShuffleArray(
+                                                possibleObstacleCoords.ToArray(), obstacleSeed
+                                            )
+                                         );
     }
 
     // Create an empty game object to hold our map, and nest it under our Map game object
@@ -109,6 +199,7 @@ public class MapGenerator : MonoBehaviour {
         mapHolder.parent = transform;
         return mapHolder;
     }
+
     void DestroyExistingMapHolder() {
         Transform existingMapHolder = transform.Find(mapHolderName);
         if (existingMapHolder) {
@@ -117,7 +208,7 @@ public class MapGenerator : MonoBehaviour {
     }
 
     public Coord GetRandomCoord() {
-        Coord randomCoord = shuffledTileCoords.Dequeue();
+        Coord randomCoord = shuffledPossibleObstacleCoords.Dequeue();
         return randomCoord;
     }
 
@@ -128,6 +219,14 @@ public class MapGenerator : MonoBehaviour {
         public Coord(int _x, int _y) {
             x = _x;
             y = _y;
+        }
+
+        public static bool operator == (Coord c1, Coord c2) {
+            return c1.x == c2.x && c1.y == c2.y;
+        }
+
+        public static bool operator != (Coord c1, Coord c2) {
+            return !(c1 == c2);
         }
     }
 }
