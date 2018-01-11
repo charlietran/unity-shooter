c8b95154b3a28b83f35214a38369b6153f0763e3
Charlie Tran <hi@charlietran.com>
2018-01-10 23:33:13 -0500

    Set Cinemachine Camera to follow player

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+	public Transform virtualCameraTransform;
+	public Transform playerTransform;
+	Cinemachine.CinemachineVirtualCamera virtualCamera;
+
+	void Awake() {
+		virtualCamera = virtualCameraTransform.GetComponent<Cinemachine.CinemachineVirtualCamera>();
+		virtualCamera.Follow = playerTransform;
+	}
+}
