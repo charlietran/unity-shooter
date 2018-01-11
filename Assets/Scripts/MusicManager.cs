a6ac2519038bb27d97799f024b36e17f50315386
Charlie Tran <hi@charlietran.com>
2018-01-11 04:08:26 -0500

    Play music based on scene

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,12 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour {
-	public AudioClip mainTheme;
-	public AudioClip menuTheme;
+    public AudioClip mainTheme;
+    public AudioClip menuTheme;
 
-	void Start() {
-		AudioManager.instance.PlayMusic(menuTheme, 2);
+    string activeSceneName;
+
+    void Start() {
+    }
+
+	void OnEnable() {
+		SceneManager.sceneLoaded += OnSceneLoaded;
 	}
+
+	void OnDisable() {
+		SceneManager.sceneLoaded -= OnSceneLoaded;
+	}
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+		if (scene.name != activeSceneName) {
+			activeSceneName = scene.name;
+			Invoke("PlayMusic", 0.2f);
+		}
+
+    }
+
+    void PlayMusic() {
+        AudioClip clip = null;
+        if (activeSceneName == "Menu") {
+            clip = menuTheme;
+        } else if (activeSceneName == "Game") {
+            clip = mainTheme;
+        }
+
+        if (clip != null) {
+            AudioManager.instance.PlayMusic(clip, 2);
+			Invoke("PlayMusic", clip.length);
+        }
+    }
+
 }
